bcca9a3f2312e77df9f6356d9a846d32d04cb539
IMneira <imneira@miuandes.cl>
2025-06-15 03:52:45 -0400

    Implement main application logic for query handling and database interaction

---
+++ app.py
@@ -0,0 +1,52 @@
+import argparse
+from langchain_chroma import Chroma
+from langchain.prompts import ChatPromptTemplate
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+from get_embedding import get_embedding
+
+CHROMA_PATH = "chroma"
+
+PROMPT_TEMPLATE = """
+Responde la siguiente pregunta basándote solamente en el contexto que se te proporciona a continuacion. Si no encuentras información relevante, responde con "No puedo responder a esa pregunta".:
+
+{context}
+
+---
+
+Responde la pregunta basándote en el contexto anterior: {question}
+"""
+
+
+def main():
+    # Create CLI.
+    parser = argparse.ArgumentParser()
+    parser.add_argument("query_text", type=str, help="The query text.")
+    args = parser.parse_args()
+    query_text = args.query_text
+    query_rag(query_text)
+
+
+def query_rag(query_text: str):
+    # Prepare the DB.
+    embedding_function = get_embedding()
+    db = Chroma(persist_directory=CHROMA_PATH, embedding_function=embedding_function)
+
+    # Search the DB.
+    results = db.similarity_search_with_score(query_text, k=5)
+
+    context_text = "\n\n---\n\n".join([doc.page_content for doc, _score in results])
+    prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
+    prompt = prompt_template.format(context=context_text, question=query_text)
+
+    model = ChatGoogleGenerativeAI(model="gemini-1.5-flash", google_api_key="AIzaSyCAEzhzpI-XmKx9oFr_2ltplEUHperCaew")
+    response_text = model.invoke(prompt)
+
+    sources = [doc.metadata.get("id", None) for doc, _score in results]
+    formatted_response = f"Response: {response_text}\nSources: {sources}"
+    print(formatted_response)
+    return response_text
+
+
+if __name__ == "__main__":
+    main()
